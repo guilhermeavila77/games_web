329f7a30ad88b21c441e2e7e137daaf17d9c1081
guilhermeavila77 <guialmeidaavila@gmail.com>
2022-08-27 23:35:22 -0300

    Definindo variaveis

--- NokiaSnake/app.js
+++ NokiaSnake/app.js
@@ -1,1 +1,17 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const squares = document.querySelectorAll('.grid div')
+    const scoreDisplay = document.querySelector('span')
+    const startBtn = document.querySelector('.start')
 
+    const width = 10
+    let currentIndex = 0
+    let appleIndex = 0
+    let currentSnake = [2,1,0]
+
+    let direction = 1
+    let score = 0 
+    let speed = 0.9
+    let intervaalTime = 0
+    let interval = 0
+
+})
